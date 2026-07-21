90182b018c4ad3a0fd5a89666ba5c48858ba3a54
Powerful AI Admin <admin@powerfulai.com>
2026-07-21 10:56:33 +0300

    Fix Microsoft logo SVG

--- src/components/LoginScreen.jsx
+++ src/components/LoginScreen.jsx
@@ -87,7 +87,12 @@ export default function LoginScreen() {
             disabled={isLoading}
             className="w-full flex items-center justify-center gap-3 bg-black/5 hover:bg-black/10 border border-black/10 dark:bg-white/5 dark:hover:bg-white/10 dark:border-white/10 text-textMain py-3 px-4 rounded-xl transition-all duration-200 shadow-sm disabled:opacity-50"
           >
-            <img src="https://www.svgrepo.com/show/452296/microsoft.svg" className="w-5 h-5" alt="Microsoft" />
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 21 21" className="w-5 h-5">
+              <path fill="#f25022" d="M1 1h9v9H1z"/>
+              <path fill="#00a4ef" d="M1 11h9v9H1z"/>
+              <path fill="#7fba00" d="M11 1h9v9h-9z"/>
+              <path fill="#ffb900" d="M11 11h9v9h-9z"/>
+            </svg>
             Login with Microsoft
           </button>
         </div>
